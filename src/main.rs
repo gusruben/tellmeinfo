accb48a1180ad53e6b61f22764c00e8da1f8fa10
Gus Ruben <gusruben@protonmail.com>
2024-12-31 22:52:17 -0500

    Fix bugs and add custom command support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{thread::sleep, time};
+use std::{thread::sleep, time, process::Command as SystemCommand};
 
 use clap::{Parser, Subcommand};
+use shlex::Shlex;
 use sysinfo::System;
 
 #[derive(Parser, Debug)]
@@ -24,6 +25,9 @@ enum Command {
         #[arg(short, long, default_value_t = 0)]
         decimals: usize,
     },
+    Custom {
+        command: String,
+    }
 }
 
 
@@ -31,25 +35,40 @@ fn main() {
     let args = Args::parse();
 
     // initialize everything
-    let mut sys;
+    let mut sys: Option<System> = None;
+    let mut program: Option<String> = None;
+    let mut custom_command_args: Option<Vec<String>> = None;
     match args.command {
         Command::RAM { .. } | Command::CPU { .. } => {
-            sys = System::new();
+            sys = Some(System::new());
         },
+        Command::Custom { ref command } => {
+            let mut parts = Shlex::new(command.as_str());
+            program = Some(parts.next().unwrap());
+            custom_command_args = Some(parts.collect());
+        }
     }
     
     loop {
-        sleep(time::Duration::from_secs(args.interval));
-
         match args.command {
             Command::RAM { decimals } => {
+                let sys = sys.as_mut().unwrap();
                 sys.refresh_memory();
                 println!("{:.1$} GiB", (sys.used_memory() as f64) / 1_000_000_000.0, decimals);
             },
             Command::CPU { decimals } => {
+                let sys = sys.as_mut().unwrap();
                 sys.refresh_cpu_usage();
                 println!("{:.1$}%", sys.global_cpu_usage(), decimals);
             },
+            Command::Custom { ref command } => {
+                SystemCommand::new(program.as_ref().unwrap())
+                .args(custom_command_args.as_ref().unwrap())
+                .status()
+                .expect(format!("Failed to execute: {}", command).as_str());
+            }
         }
+
+        sleep(time::Duration::from_secs(args.interval));
     }
 }
